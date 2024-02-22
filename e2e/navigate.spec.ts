e4a597d7a6e44b7d0408210fcd440c3258b8e58c
k31tha <keith@kdasoft.co.uk>
2024-02-22 00:15:19 +0000

    Added basic playwright tests with navigation  and router

---
+++ e2e/navigate.spec.ts
@@ -0,0 +1,46 @@
+import {test, expect} from '@playwright/test';
+
+test('root page of app', async ({page}) => {
+  await page.goto('http://localhost:5173/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/CWK - Project NLS/);
+  await expect(page.locator('#detail').getByText('home')).toBeVisible();
+});
+
+test('club search page of app', async ({page}) => {
+  await page.goto('http://localhost:5173/club');
+
+  // Expect a title "to contain" a substring.
+  //await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.getByText('club search')).toBeVisible();
+});
+
+test('club page of app for a club', async ({page}) => {
+  await page.goto('http://localhost:5173/club/woking-fc');
+
+  // Expect a title "to contain" a substring.
+  //await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.getByText('club with name')).toBeVisible();
+});
+
+test('navigate down to a club from route page', async ({page}) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page).toHaveTitle(/CWK - Project NLS/);
+  await expect(page.locator('#detail').getByText('home')).toBeVisible();
+  await page.click('text=club');
+  await expect(page.getByText('club search')).toBeVisible();
+  //await page.click('text=club/woking-fc');
+  await page.getByRole('link', {name: 'woking fc'}).click();
+  await expect(page.getByText('club with name')).toBeVisible();
+});
+
+//test('get started link', async ({page}) => {
+//await page.goto('https://playwright.dev/');
+
+// Click the get started link.
+//await page.getByRole('link', {name: 'Get started'}).click();
+
+// Expects page to have a heading with the name of Installation.
+//await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
+//});
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import {createBrowserRouter, RouterProvider, Link} from 'react-router-dom';
+import ErrorPage from './components/error/ErrorPage';
+//import './index.css';
+import Root from './components/routes/route';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <div>home</div>,
+      },
+      {
+        path: 'club/:id',
+        element: <div>club with name</div>,
+      },
+      {
+        path: 'club',
+        element: (
+          <>
+            <div>club search</div>
+            <ul>
+              <li>
+                <Link to={`/club/woking-fc`}>woking fc</Link>
+              </li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        path: 'blog',
+        element: <div>blog</div>,
+      },
+    ],
+  },
+]);
+
+//just creates  basic routing for the app. The first route is the root route and the second route is the club route. The element prop is the component that will be rendered when the route is matched. The path prop is the path that will be matched to the current URL.
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
