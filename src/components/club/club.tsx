da66fe42731f334fda74d2f4b6aefc881a07cca7
k31tha <keith@kdasoft.co.uk>
2024-05-16 23:04:09 +0100

    Show club details on page

--- src/components/club/club.tsx
+++ src/components/club/club.tsx
@@ -16,13 +16,65 @@ export const Club: React.FC = () => {
     });
   }, [urlFriendlyName]);
   if (!clubDetail) {
-    return <div>Loading Club...</div>;
+    return <div>Club not found</div>;
+  } else if (clubDetail.ClubID === 0) {
+    return <div>Club not found</div>;
   }
   //const {clubDetail} = await getClubDetails(urlFriendlyName as string);
   return (
-    <div>
-      <h1>{clubDetail.ClubName}</h1>
-      <ClubLogo logoUrl={clubDetail.ClubLogo ?? undefined} />
-    </div>
+    <>
+      <section id="club-main" className={'mt-5'}>
+        <section
+          className={'mt-5 flex flex-col items-center sm:flex-row gap-5'}
+        >
+          <section id="club-details" className={'order-last'}>
+            <h1 className={'hidden sm:block'}>{clubDetail.ClubName}</h1>
+            <p className={'mt-5'}>
+              {clubDetail.ClubName} also known as {clubDetail.Nicknames} are a
+              current team in the non league pyramid. They are currently members
+              of the (league - need different data or change api).
+            </p>
+            <p>
+              They currently play their matches at {clubDetail.ClubAddress} †
+            </p>
+            <p>
+              For more information see the club website{' '}
+              <a target="_blank" href={clubDetail.MainWebsite ?? 'tbc'}>
+                click here
+              </a>{' '}
+              or wikipedia{' '}
+              <a target="_blank" href={clubDetail.ClubWikiLink ?? 'tbc'}>
+                click here
+              </a>
+            </p>
+          </section>
+          {clubDetail.ClubLogo ? (
+            <ClubLogo
+              logoUrl={clubDetail.ClubLogo}
+              clubName={clubDetail.ClubName ?? ''}
+            />
+          ) : (
+            <></>
+          )}
+        </section>
+      </section>
+      <section id="club-social">
+        <h2>Social Media</h2>
+        <p className={'italic'}>(to flesh out from different object)</p>
+        <ul></ul>
+      </section>
+      <p className={'mt-5 italic'}>
+        † nonleaguesocial.co.uk does not guarantee or warrant the accuracy or
+        reliability of any information{' '}
+      </p>
+    </>
   );
 };
+
+/*
+{clubDetail.SocialMedia.map(socialMedia => (
+          <li key={socialMedia.SocialMediaId}>
+            <a href={socialMedia.Url}>{socialMedia.Name}</a>  
+          </li>
+        ))}
+*/
--- src/components/club/clubLogo.tsx
+++ src/components/club/clubLogo.tsx
@@ -1,9 +1,12 @@
-export const ClubLogo: React.FC<{logoUrl?: string | undefined}> = ({
+export const ClubLogo: React.FC<{logoUrl: string; clubName: string}> = ({
   logoUrl,
+  clubName,
 }) => {
   return (
     <section id="clubLogo">
       <img
+        className={'p-5 max-w-32 h-auto'}
+        alt={`${clubName} Club Logo`}
         src={`https://nonleaguesocial.co.uk/assets/images/club/logo/${logoUrl}`}
       />
     </section>
